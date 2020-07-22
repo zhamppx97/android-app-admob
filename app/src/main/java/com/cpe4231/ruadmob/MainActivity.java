80e693c71eba23f5000470fc29650409cf276d88
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2020-07-22 13:20:20 +0700

    init commit

---
+++ app/src/main/java/com/cpe4231/ruadmob/MainActivity.java
@@ -0,0 +1,19 @@
+package com.cpe4231.ruadmob;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
